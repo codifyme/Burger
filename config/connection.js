f7f8fa55677ed32ed59e55a4047cb93ae574e6e6
Angel Cochachez <acochachez@gmail.com>
2019-11-03 10:58:17 -0800

    Fixing JAWS_URL and adding config.json

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,16 @@
 const mysql = require('mysql');
 var connection;
 
-const dotenv = require('dotenv');
-dotenv.config();
-const inquirer = require('inquirer');
-
-const MYSQL_PASS = process.env.MYSQL_PASS;
-
-if (process.env.JAWSDB_URL) {
+if (process.env.JAWSDB_YELLOW_URL) {
   //Database is JawsDB on Heroku
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_YELLOW_URL);
 } else {
   //Database is local
   connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: 'MYSQL_PASS',
+    password: 'marcelperu2019',
     database: 'burgers_db'
   });
 }
